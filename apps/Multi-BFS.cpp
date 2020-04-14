52e0cd82e820de13cd5b3495d56b3e1f71e78df1
chhzh123 <982270930@qq.com>
2020-04-15 00:05:17 +0800

    Add Multi-BFS

--- apps/Multi-BFS.cpp
+++ apps/Multi-BFS.cpp
@@ -2,6 +2,9 @@
 // Copyright (c) 2019 Hongzheng Chen
 
 #include "krill.h"
+#include "Multi-BFS.pb.h"
+using namespace MultiBFS;
+
 #include "BFS.h"
 using namespace std;
 
@@ -10,8 +13,10 @@ void setKernels(graph<vertex>&G, Kernels& K, commandLine P)
 {
 	long cnt = P.getOptionLongValue("-n",1);
 	cout << cnt << " BFSs" << endl;
+	MultiBFS::Property prop(G.n);
 	for (int i = 1; i < cnt+1; ++i){
-		BFS* bfs = new BFS(G.n,10*i); // remember to dynamically allocate memory
+		BFS* bfs = new BFS(G.n, prop, 10*i); // remember to dynamically allocate memory
 		K.appendJob(bfs);
 	}
+	prop.initialize();
 }
