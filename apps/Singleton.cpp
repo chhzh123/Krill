eaa1c1ad120aad0d41855553cdf9efb2022acadb
chhzh123 <982270930@qq.com>
2020-04-18 21:08:25 +0800

    Add Singleton compilation

--- apps/Singleton.cpp
+++ apps/Singleton.cpp
@@ -2,6 +2,9 @@
 // Copyright (c) 2019 Hongzheng Chen
 
 #include "krill.h"
+#include "Heter.pb.h"
+using namespace Heter;
+
 #include "BFS.h"
 #include "CC.h"
 #include "PageRank.h"
@@ -12,25 +15,27 @@ using namespace std;
 template <class vertex>
 void setKernels(graph<vertex>&G, Kernels& K, commandLine P)
 {
-	string job_name = P.getOptionValue("-job","bfs");
+	Heter::PropertyManager prop(G.n);
+	string job_name = P.getOptionValue("-job", "bfs");
 	long start = P.getOptionLongValue("-r",0);
 	if (job_name == "bfs"){
-		BFS* bfs = new BFS(G.n,start);
+		BFS* bfs = new BFS(G.n, prop, start);
 		K.appendJob(bfs);
 	} else if (job_name == "cc"){
-		Components* cc = new Components(G.n);
+		Components* cc = new Components(G.n, prop);
 		K.appendJob(cc);
 	} else if (job_name == "pr"){
-		PageRank<vertex>* pr = new PageRank<vertex>(G.n,G.V);
+		PageRank<vertex>* pr = new PageRank<vertex>(G.n, G.V);
 		K.appendJob(pr);
 	} else if (job_name == "sssp"){
-		SSSP* sssp = new SSSP(G.n,start);
+		SSSP* sssp = new SSSP(G.n, prop, start);
 		K.appendJob(sssp);
 	} else if (job_name == "prd"){
-		PageRankDelta<vertex>* prd = new PageRankDelta<vertex>(G.n,G.V);
+		PageRankDelta<vertex>* prd = new PageRankDelta<vertex>(G.n, G.V, prop);
 		K.appendJob(prd);
 	} else {
 		cerr << "Error: No this job!" << endl;
 		abort();
 	}
+	prop.initialize();
 }
